e3fd27a0c40256dd795f3bfc575d00910849070c
Sandip Thapa <sandipthapa386@gmail.com>
2025-01-31 22:39:27 +0545

    feat: Clerk setup for authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from '@clerk/clerk-react';
 import AppRoutes from './routes';
+import { Suspense } from 'react';
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <Suspense fallback={<p>Loading...</p>}>
+          <AppRoutes />
+          <UserButton />
+        </Suspense>
+      </SignedIn>
+    </>
+  );
 }
 
 export default App;
